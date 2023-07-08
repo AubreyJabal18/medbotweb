6dd7c84a848754bd50d782c4f74a530af69a9746
Aubrey_Jabal18 <aubreyjabal12345@gmail.com>
2023-07-08 11:49:46 +0800

    added login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Libern\QRCodeReader\QRCodeReader;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    //
+    public function loginByUpload(Request $request){
+      
+        $QRCodeReader = new QRCodeReader();
+        $qrcode_text = $QRCodeReader->decode($request->file('qrcode'));
+        $qrcode_data = explode(' ', $qrcode_text);
+        $type = $qrcode_data[0];
+
+        if($type != 'medbot'){
+            flash()->addError('Invalid QRCode');
+            return back();
+        }
+
+        $id = $qrcode_data[1];
+        $password = $qrcode_data[2];
+
+        $credentials = [
+            'id' => $id,
+            'password' => $password
+        ];
+
+        if(Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            flash()->addSuccess('Login successfully');
+            return redirect()->intended('dashboard');
+        }
+
+        dd($credentials[0]);
+
+    }
+
+}
