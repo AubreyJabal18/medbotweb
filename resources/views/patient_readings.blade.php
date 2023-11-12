a7934ef1ecc45bbb782267437df5072c1d9530a5
Oreo0505 <rylenesacro@gmail.com>
2023-11-12 23:11:49 +0800

    new download pic

--- resources/views/patient_readings.blade.php
+++ resources/views/patient_readings.blade.php
@@ -118,7 +118,7 @@
 
 		<div class="fixed bottom-2 right-10 z-50 md:bottom-6 ">
 			<div id="download-reading-button" class="flex flex-row items-center justify-center p-2 space-x-2 w-40 h-16 border border-white bg-[#5EDAD5] rounded-xl shadow-sm shadow-black transition-colors duration-300 hover:bg-[#4ac0b8] focus:bg-[#4ac0b8]">
-				<img src="{{asset('images/about/vector.png')}}" alt="" class="w-8 h-8">
+				<img src="{{asset('images/downloadbtn.png')}}" alt="" class="w-8 h-8">
 				<button class="text-roboto font-medium text-sm text-black text-center cursor-pointer">Download Vital Signs Reading History </button>
 			</div>
 		</div>
