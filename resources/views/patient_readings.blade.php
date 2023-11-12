39104f6564f3f84fdf2977807690fdf22fa514ab
Oreo0505 <rylenesacro@gmail.com>
2023-11-12 11:45:30 +0800

    added medbot pic to other pages

--- resources/views/patient_readings.blade.php
+++ resources/views/patient_readings.blade.php
@@ -22,7 +22,7 @@
 		<div class="flex flex-row justify-between items-center px-4 py-3 md:px- mb-6 z-10">
 			<div class="flex flex-row space-x-4 items-center">
 				<a href="/">
-					<img src={{asset('images/dashboard/logo2.png')}} alt="" class="h-8 w-8 md:h-12 md:w-12">
+					<img src={{asset('images/LOGO.png')}} alt="" class="h-8 w-8 md:h-12 md:w-12">
 				</a>
 				<p class="font-roboto font-bold text-[#07DBDB] text-lg md:text-2xl text-shadow">ENHANCED MED-BOT</p>
 			</div>
@@ -46,15 +46,15 @@
 		<div class="flex flex-col space-y-2 md:space-y-0 md:flex-row items-center justify-center md:space-x-5 relative">
 			
 			<div class="flex items-center">
-				<img src="{{asset('images/dashboard/heartbeat.svg')}}" alt="" class="absolute w-36 h-40 xl:w-[600px] xl:h-40 left-0 top-56 xl:top-2 md:hidden xl:block">
+				<img src="{{asset('images/dashboard/heartbeat.svg')}}" alt="" class="absolute w-36 h-40 xl:w-[600px] xl:h-40 left-0 lg:left-32 xl:left-9 top-56 lg:-top-1 xl:-top-1 md:hidden lg:block">
 				<img src="{{asset('images/dashboard/heartbeat.svg')}}" alt="" class="absolute w-36 h-40 xl:w-auto xl:h-40 left-0 hidden md:block">
-				<img src="{{asset('images/dashboard/heartbeat.svg')}}" alt="" class="absolute w-36 h-40 xl:w-[640px] xl:h-40 right-0 top-56 xl:top-2 md:hidden xl:block">
+				<img src="{{asset('images/dashboard/heartbeat.svg')}}" alt="" class="absolute w-36 h-40 xl:w-[640px] xl:h-40 right-0 lg:right-32 xl:right-4 top-56 lg:-top-1 xl:-top-1 md:hidden lg:block">
 				<img src="{{asset('images/dashboard/heartbeat.svg')}}" alt="" class="absolute w-36 h-40 xl:w-auto xl:h-40 right-0 hidden md:block">
 			</div>
 
 			<div class="flex flex-row items-center justify-center space-x-2 md:space-x-5">
 				<div class="flex flex-col relative items-center p-2 rounded-xl drop-shadow-lg shadow-lg border border-gray-300 h-[100px] lg:h-[150px] w-[150px] lg:w-[200px] space-y-2 bg-blue-200" title="This shows the times you used the Enhanced Med-Bot for this week.">
-					<p class="flex font-roboto font-normal text-black text-sm xl:text-xl md:text-base">Weekly Count</p>
+					<p class="flex font-roboto font-normal text-black text-sm xl:text-xl md:text-base">Week Count</p>
 					
 					<div class="flex flex-row w-full h-fit items-center justify-evenly">
 						<img src="{{asset('images/dashboard/BP.svg')}}" alt=""  class="w-12 lg:w-24 h-12 lg:h-24">
@@ -64,7 +64,7 @@
 				</div>
 
 				<div class="flex flex-col items-center p-2 rounded-xl drop-shadow-lg shadow-lg border border-gray-300 h-[100px] lg:h-[150px] w-[150px] lg:w-[200px] space-y-2 bg-blue-200" title="This shows the times you used the Enhanced Med-Bot for this month.">
-					<p class="flex font-roboto font-normal text-black text-sm md:text-base xl:text-xl">Monthly Count</p>
+					<p class="flex font-roboto font-normal text-black text-sm md:text-base xl:text-xl">Month Count</p>
 					
 					<div class="flex flex-row w-full h-fit items-center justify-evenly">
 						<img src="{{asset('images/dashboard/BP.svg')}}" alt="" class="w-12 lg:w-24 h-12 lg:h-24">
@@ -76,7 +76,7 @@
 			</div>
 
 			<div class="flex flex-col items-center p-2 rounded-xl drop-shadow-lg shadow-lg border border-gray-300 h-[100px] lg:h-[150px] w-[150px] lg:w-[200px] space-y-2 bg-blue-200" title="This shows the times you used the Enhanced Med-Bot for this year.">
-            	<p class="flex font-roboto font-normal text-black text-sm md:text-base xl:text-xl">Yearly Count</p>
+            	<p class="flex font-roboto font-normal text-black text-sm md:text-base xl:text-xl">Year Count</p>
 				
 				<div class="flex flex-row w-full h-fit items-center justify-evenly">
 					<img src="{{asset('images/dashboard/BP.svg')}}" alt="" class="w-12 lg:w-24 h-12 lg:h-24">
@@ -85,7 +85,7 @@
 				<img src="{{asset('images/dashboard/bg_design.png')}}" alt="" class=" absolute w-16 lg:w-24 h-16 lg:h-24 bottom-0 left-0 -z-10 rounded-xl ">
             </div>
 			
-			<img src="{{asset('images/dashboard/model2.png')}}" alt="" class="w-36 h-36 lg:w-44 lg:h-44 z-10">
+			<img src="{{asset('images/side view.png')}}" alt="" class="w-36 h-44 md:w-36 md:h-48 lg:w-44 lg:h-52 xl:w-52 xl:h-64 md:hidden z-10">
 			
 		</div>
 		
@@ -118,8 +118,8 @@
 
 		<div class="fixed bottom-2 right-10 z-50 md:bottom-6 ">
 			<div id="download-reading-button" class="flex flex-row items-center justify-center p-2 space-x-2 w-40 h-16 border border-white bg-[#5EDAD5] rounded-xl shadow-sm shadow-black transition-colors duration-300 hover:bg-[#4ac0b8] focus:bg-[#4ac0b8]">
-				<img src="{{asset('images/dashboard/download.png')}}" alt="" class="w-12 h-12">
-				<button class="text-roboto font-medium text-base text-black text-center cursor-pointer">Download Reading</button>
+				<img src="{{asset('images/about/vector.png')}}" alt="" class="w-8 h-8">
+				<button class="text-roboto font-medium text-sm text-black text-center cursor-pointer">Download Vital Signs Reading History </button>
 			</div>
 		</div>
 		
